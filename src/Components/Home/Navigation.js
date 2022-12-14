4bfb2fca2147428cb50918122cbb475e0eb25a4c
SumiAkter2 <akteralisha36@gmail.com>
2022-12-14 17:58:50 +0600

    re-arrange all

--- src/Components/Home/Navigation.js
+++ src/Components/Home/Navigation.js
@@ -5,55 +5,108 @@ import Form from "react-bootstrap/Form";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { useAuthState, useSignOut } from "react-firebase-hooks/auth";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import auth from "../../Firebase/firebase.init";
 
 const Navigation = () => {
   const [signOut] = useSignOut(auth);
   const [user] = useAuthState(auth);
   return (
-    <Navbar bg="light" expand="lg">
-      <Container fluid>
-        <NavLink to="/" className="fs-3 fw-bold">
-          Jwt Implement
-        </NavLink>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: "100px" }}
-            navbarScroll
-          ></Nav>
-          <NavLink to="/" className="me-2 fw-bold">
-            Home
-          </NavLink>
-          <Navbar className="ms-5 me-5">{user?.displayName} </Navbar>
+    // <Navbar bg="light" expand="lg">
+    //   <Container fluid>
+    //     <NavLink to="/" className="fs-3 fw-bold">
+    //       Jwt Implement
+    //     </NavLink>
+    //     <Navbar.Toggle aria-controls="navbarScroll" />
+    //     <Navbar.Collapse id="navbarScroll">
+    //       <Nav
+    //         className="me-auto my-2 my-lg-0"
+    //         style={{ maxHeight: "100px" }}
+    //         navbarScroll
+    //       ></Nav>
+    //       <NavLink to="/" className="me-2 fw-bold">
+    //         Home
+    //       </NavLink>
+    //       <Navbar className="ms-5 me-5">{user?.displayName} </Navbar>
 
-          {user ? (
-            <NavLink
-              to="/login"
-              className="me-2 fw-bold"
-              onClick={() => signOut()}
-            >
-              Sign Out
-            </NavLink>
-          ) : (
-            <NavLink to="/login" className="me-2 fw-bold">
-              Log In
-            </NavLink>
-          )}
-          <Form className="d-flex ">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">Search</Button>
-          </Form>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    //       {user ? (
+    //         <NavLink
+    //           to="/login"
+    //           className="me-2 fw-bold"
+    //           onClick={() => signOut()}
+    //         >
+    //           Sign Out
+    //         </NavLink>
+    //       ) : (
+    //         <NavLink to="/login" className="me-2 fw-bold">
+    //           Log In
+    //         </NavLink>
+    //       )}
+    //       <Form className="d-flex ">
+    //         <Form.Control
+    //           type="search"
+    //           placeholder="Search"
+    //           className="me-2"
+    //           aria-label="Search"
+    //         />
+    //         <Button variant="outline-success">Search</Button>
+    //       </Form>
+    //     </Navbar.Collapse>
+    //   </Container>
+    // </Navbar>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container">
+        <Link className="navbar-brand" to="/">
+          Random User
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarText"
+          aria-controls="navbarText"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarText">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link" aria-current="page" to="/home">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/users">
+                Users
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/login">
+                Login
+              </Link>
+            </li>
+          </ul>
+          <span className="navbar-text">
+            {!user ? (
+              "USER"
+            ) : (
+              <span>
+                {user.email}
+                <button
+                  onClick={() => signOut(auth)}
+                  type="button"
+                  className="btn btn-light"
+                >
+                  Sign Out
+                </button>
+              </span>
+            )}
+          </span>
+        </div>
+      </div>
+    </nav>
   );
 };
 
--- src/Components/LogIn/SignIn.js
+++ src/Components/LogIn/SignIn.js
@@ -1,119 +1,20 @@
 import React from "react";
-import { useState } from "react";
 import "./Login.css";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
-import { AiFillEye } from "react-icons/ai";
-import { AiFillEyeInvisible } from "react-icons/ai";
-import Google from "./Google";
-import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
-import auth from "../../Firebase/firebase.init";
-import { useNavigate } from "react-router-dom";
 const SignIn = () => {
-  const [showPassword, setShowPassword] = useState(false);
-  const [email, setEmail] = useState({ value: "", error: "" });
-  const [password, setPassword] = useState({ value: "", error: "" });
-  const [createUserWithEmailAndPassword, user, loading, error] =
-    useCreateUserWithEmailAndPassword(auth);
-  const navigate = useNavigate();
-  const passwordHandle = (e) => {
-    const passwordInput = e.target.value;
-    //   const pass='^(?=.*[A-Za-z])(?=.*d)[A-Za-zd]{8,}$'
-    if (/.{6,}/.test(passwordInput)) {
-      setPassword({
-        value: passwordInput,
-        error: "",
-      });
-    } else {
-      setPassword({
-        value: "",
-        error: "Please provide minimum 8 characters",
-      });
-    }
-  };
-  const emailHandle = (e) => {
-    const emailInput = e.target.value;
-    if (/^\S+@\S+\.\S+$/.test(emailInput)) {
-      setEmail({ value: emailInput, error: "" });
-    } else {
-      setEmail({ value: "", error: "Please Provide a valid Email" });
-    }
-  };
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (email.value === "") {
-      setEmail({ value: "", error: "Email is required" });
-    }
-    if (password.value === "") {
-      setPassword({ value: "", error: "Password is required" });
-    }
-    createUserWithEmailAndPassword(email, password);
-    console.log(email, password);
-  };
-  if (user) {
-    navigate("/");
-  }
   return (
-    <div className="w-50 mx-auto">
-      <h1>Sign Up</h1>
-      <Form onClick={handleSubmit}>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="Enter email"
-            onBlur={emailHandle}
-          />
-          {email.error && (
-            <Form.Text className="text-danger fw-bold">{email.error}</Form.Text>
-          )}
-        </Form.Group>
-
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <p className="d-flex ">
-            <Form.Control
-              type={showPassword ? "text" : "password"}
-              placeholder="Password"
-              onBlur={passwordHandle}
-            />
-
-            <span>
-              {showPassword ? (
-                <AiFillEye
-                  className="icon"
-                  size="30px"
-                  onClick={() => setShowPassword(!showPassword)}
-                />
-              ) : (
-                <AiFillEyeInvisible
-                  className="icon "
-                  size="30px"
-                  onClick={() => setShowPassword(!showPassword)}
-                />
-              )}
-            </span>
-          </p>
-          {password.error && (
-            <Form.Text className="text-danger fw-bold">
-              {password.error}
-            </Form.Text>
-          )}
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          {/* <Form.Check type="checkbox" label="Check me out" /> */}
-        </Form.Group>
-        {email.value && password.value ? (
-          <Button variant="primary" type="submit">
-            Sign Up
-          </Button>
-        ) : (
-          <Button disabled variant="primary">
-            Sign Up
-          </Button>
-        )}
-      </Form>
-      <Google />
+    <div>
+      <div class="inputbox">
+        <input required="required" type="text" />
+        <span>Username</span>
+        <i></i>
+      </div>
+      <br />
+      <br />
+      <div class="inputbox">
+        <input required="required" type="text" />
+        <span>Phone</span>
+        <i></i>
+      </div>
     </div>
   );
 };
